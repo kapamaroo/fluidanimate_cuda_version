b308cea325e11c8b561e2a52bc27c5ded20cea0a
Maroudas Manolis <kapamaroo@gmail.com>
2012-04-02 08:42:43 +0300

    rename run_tests.sh to test.sh
    also change some parameters to run on my desktop

--- bin/test.sh
+++ bin/test.sh
@@ -6,8 +6,8 @@ LOGDIR="log"
 CPU_BIN="fluidanimate_cpu"
 GPU_BIN="fluidanimate_gpu"
 
-THREADS="16"
-FRAMES="64"
+THREADS="256"
+FRAMES="1"
 
 INPUT="./inputs/in_100K.fluid"
 CPU_OUTPUT="./out_100K_cpu.fluid"
