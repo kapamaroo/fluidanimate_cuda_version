3adf113d71aa016a07f72272f91e34e355aa92e0
Maroudas Manolis <kapamaroo@gmail.com>
2012-04-04 02:56:40 +0300

    now test.sh takes as parameter the number of frames

--- tools/test.sh
+++ tools/test.sh
@@ -7,7 +7,7 @@ CPU_BIN="fluidanimate_cpu"
 GPU_BIN="fluidanimate_gpu"
 
 THREADS="256"
-FRAMES="1"
+FRAMES="$@"
 
 INPUT="inputs/in_100K.fluid"
 CPU_OUTPUT="out_100K_cpu.fluid"
@@ -15,5 +15,20 @@ GPU_OUTPUT="out_100K_gpu.fluid"
 
 mkdir -p $LOGDIR
 
-#./$BUILDDIR/$CPU_BIN $THREADS $FRAMES ./$INPUT ./$LOGDIR/$CPU_OUTPUT
+echo "###"
+echo "#"
+echo "# Start Test for $@ frames"
+echo "#"
+echo "# CPU Test"
+echo "#"
+
+./$BUILDDIR/$CPU_BIN $THREADS $FRAMES ./$INPUT ./$LOGDIR/$CPU_OUTPUT
+
+echo "#"
+echo "# GPU Test"
+echo "#"
+
 ./$BUILDDIR/$GPU_BIN $THREADS $FRAMES ./$INPUT ./$LOGDIR/$GPU_OUTPUT
+
+echo "#"
+echo "# Test Finished."
