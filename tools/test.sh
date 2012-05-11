b236026312003bbb5630c0ed4da893e70c2e0059
Maroudas Manolis <kapamaroo@gmail.com>
2012-05-11 22:20:09 +0300

    polish test output

--- tools/test.sh
+++ tools/test.sh
@@ -6,6 +6,8 @@ LOGDIR="log"
 CPU_BIN="fluidanimate_cpu"
 GPU_BIN="fluidanimate_gpu"
 
+CHECK_BIN="./tools/checkfiles"
+
 THREADS="256"
 FRAMES="$@"
 
@@ -15,20 +17,28 @@ GPU_OUTPUT="out_100K_gpu.fluid"
 
 mkdir -p $LOGDIR
 
-echo "###"
-echo "#"
-echo "# Start Test for $@ frames"
-echo "#"
-echo "# CPU Test"
-echo "#"
+echo
+echo
+echo
+echo "######## Start Test for $@ frames #############"
+echo
+echo "############### CPU Test #####################"
+echo
 
 time ./$BUILDDIR/$CPU_BIN $THREADS $FRAMES ./$INPUT ./$LOGDIR/$CPU_OUTPUT
 
-echo "#"
-echo "# GPU Test"
-echo "#"
+echo
+echo "############### GPU Test #####################"
+echo
 
 time ./$BUILDDIR/$GPU_BIN $THREADS $FRAMES ./$INPUT ./$LOGDIR/$GPU_OUTPUT
 
-echo "#"
-echo "# Test Finished."
+echo
+echo "############ Compare Results #################"
+$CHECK_BIN ./$LOGDIR/$CPU_OUTPUT ./$LOGDIR/$GPU_OUTPUT
+echo
+echo "############## Test Finished #################"
+echo
+echo
+echo
+
